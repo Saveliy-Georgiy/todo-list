a9bcb5d21f2a1c0ad409427f1cd4bef1819379ee
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-21 15:26:28 +0300

    Added state, constructor, setState

--- src/App.js
+++ src/App.js
@@ -5,25 +5,52 @@ import TodoListTasks from "./TodoListTasks.js";
 import TodoListFooter from "./TodoListFooter.js";
 
 class App extends React.Component {
-    tasks = [
-        {title: "CSS", isDone: true, priority: "low"},
-        {title: "JS", isDone: false, priority: "medium"},
-        {title: "ReactJS", isDone: false, priority: "high"},
-        {title: "Patterns", isDone: true, priority: "low" },
-    ];
-    filterValue = "Completed";
+
+    constructor(props) {
+        super(props); //воздай то что есть у констурктора
+        this.newTaskTitleRef = React.createRef();
+    }
+
+    state = { //это объект, у него свойство tasks и filterValue
+        tasks: [
+            {title: "CSS", isDone: true, priority: "low"},
+            {title: "JS", isDone: false, priority: "medium"},
+            {title: "ReactJS", isDone: false, priority: "high"},
+            {title: "Patterns", isDone: true, priority: "low"},
+        ],
+        filterValue: "All",
+    };
+
+    onAddTaskClick = () => {
+        if (this.newTaskTitleRef.current.value === '') {
+        } else {
+        let newText = this.newTaskTitleRef.current.value;
+        let newTask = {
+            title: newText,
+            isDone: false,
+            priority: "high",
+        };
+        //let newTasks = this.state.tasks.push(newTask);
+        let newTasks = [...this.state.tasks, newTask]; // spread оператор
+        this.newTaskTitleRef.current.value = "";
+        this.setState({
+            tasks: newTasks
+        });
+        }
+    };
+
     render = () => {
 
         return (
             <div className="App">
                 <div className="todoList">
-                   <TodoListHeader />
-                    <TodoListTasks tasks={this.tasks} />
-                   <TodoListFooter filterValue={this.filterValue}/>
+                     <TodoListHeader onAddTaskClick={this.onAddTaskClick} newTaskTitleRef={this.newTaskTitleRef}/>
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
-    }
+    };
 }
 
 export default App;
